79a3890395f79e6bd41af07f636ef6576d8ec094
yunTys <1223741161@qq.com>
2020-08-21 18:43:51 +0800

    建立页面和组件

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="main">
+    <Header title="我的影院" />
+    <div id="content">
+      <Login />
+    </div>
+    <Tabbar />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Tabbar from '@/components/Tabbar'
+import Login from '@/components/Login'
+export default {
+  name: 'Mine',
+  components: {
+    Header,
+    Tabbar,
+    Login
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
