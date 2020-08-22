ff924507acb5297de4d626e542d9167cb9435de1
yunTys <1223741161@qq.com>
2020-08-22 11:42:12 +0800

    获取电影列表和影院列表

--- src/components/CityList/index.vue
+++ src/components/CityList/index.vue
@@ -1,89 +1,15 @@
 <template>
   <div class="cinema_body">
     <ul>
-      <li>
-        <div>
-          <span>大地影院(澳东世纪店)</span>
-          <span class="q"><span class="price">22.9</span> 元起</span>
+      <li v-for="item in dataList" :key="item.cinemaId">
+        <div class="name-price">
+          <span>{{ item.name }}</span>
+          <span class="q"><span class="price">{{ item.lowPrice / 100 }}</span> 元起</span>
         </div>
         <div class="address">
-          <span>金州区大连经济技术开发区澳东世纪3层</span>
-          <span>1763.5km</span>
+          <span>{{ item.address }}</span>
+          <span>距离未知</span>
         </div>
-        <div class="card">
-                  <div>小吃</div>
-                  <div>折扣卡</div>
-            </div>
-      </li>
-      <li>
-        <div>
-          <span>大地影院(澳东世纪店)</span>
-          <span class="q"><span class="price">22.9</span> 元起</span>
-        </div>
-        <div class="address">
-          <span>金州区大连经济技术开发区澳东世纪3层</span>
-          <span>1763.5km</span>
-        </div>
-        <div class="card">
-                  <div>小吃</div>
-                  <div>折扣卡</div>
-            </div>
-      </li>
-      <li>
-        <div>
-          <span>大地影院(澳东世纪店)</span>
-          <span class="q"><span class="price">22.9</span> 元起</span>
-        </div>
-        <div class="address">
-          <span>金州区大连经济技术开发区澳东世纪3层</span>
-          <span>1763.5km</span>
-        </div>
-        <div class="card">
-                  <div>小吃</div>
-                  <div>折扣卡</div>
-            </div>
-      </li>
-      <li>
-        <div>
-          <span>大地影院(澳东世纪店)</span>
-          <span class="q"><span class="price">22.9</span> 元起</span>
-        </div>
-        <div class="address">
-          <span>金州区大连经济技术开发区澳东世纪3层</span>
-          <span>1763.5km</span>
-        </div>
-        <div class="card">
-                  <div>小吃</div>
-                  <div>折扣卡</div>
-            </div>
-      </li>
-      <li>
-        <div>
-          <span>大地影院(澳东世纪店)</span>
-          <span class="q"><span class="price">22.9</span> 元起</span>
-        </div>
-        <div class="address">
-          <span>金州区大连经济技术开发区澳东世纪3层</span>
-          <span>1763.5km</span>
-        </div>
-        <div class="card">
-                  <div>小吃</div>
-                  <div>折扣卡</div>
-            </div>
-      </li>
-      <li>
-        <div>
-          <span>大地影院(澳东世纪店)</span>
-          <span class="q"><span class="price">22.9</span> 元起</span>
-        </div>
-        <div class="address">
-          <span>金州区大连经济技术开发区澳东世纪3层</span>
-          <span>1763.5km</span>
-        </div>
-        <div class="card">
-                  <div>小吃</div>
-                  <div>折扣卡</div>
-            </div>
       </li>
     </ul>
   </div>
@@ -91,7 +17,25 @@
 
 <script>
 export default {
-  name: 'CityList'
+  name: 'CityList',
+  data () {
+    return {
+      dataList: []
+    }
+  },
+  mounted () {
+    this.axios({
+      url: 'https://m.maizuo.com/gateway?cityId=440300&ticketFlag=1&k=6775958',
+      headers: {
+        'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.0.4","e":"159670442842296837931009","bc":"440300"}',
+        'X-Host': 'mall.film-ticket.cinema.list'
+      }
+    }).then((res) => {
+      if(res.data.status === 0){
+        this.dataList = res.data.data.cinemas
+      }
+    })
+  }
 }
 </script>
 
@@ -103,9 +47,7 @@ export default {
 .cinema_body .q{ font-size: 11px; color:#f03d37;}
 .cinema_body .price{ font-size: 18px;}
 .cinema_body .address{ font-size: 13px; color:#666;}
+.cinema_body .address span:nth-of-type(1){ display: inline-block; width: calc(100% - 70px) }
 .cinema_body .address span:nth-of-type(2){ float:right; }
-.cinema_body .card{ display: flex;}
-.cinema_body .card div{ padding: 0 3px; height: 15px; line-height: 15px; border-radius: 2px; color: #f90; border: 1px solid #f90; font-size: 13px; margin-right: 5px;}
-.cinema_body .card div.or{ color: #f90; border: 1px solid #f90;}
-.cinema_body .card div.bl{ color: #589daf; border: 1px solid #589daf;}
+.cinema_body .name-price { display: flex; justify-content: space-between; }
 </style>
